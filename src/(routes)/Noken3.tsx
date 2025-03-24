5af78e8714546c57b09fde35fba89ee955ecdb6b
RogerCiv <rogeliocivantos@gmail.com>
2025-03-24 13:11:50 +0100

    add Noken3Page component; implement vocabulary status management and navigation for Noken3

---
+++ src/(routes)/Noken3.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import useVocabularyStatus from "../hooks/useVocabularyStatus";
+import VocabularyPageContent from "../components/VocabularyPageContent";
+import { dataN3 } from "../data/dataN3";
+
+const LEVEL_N3 = 'Noken3';
+
+export default function Noken3Page() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentCard = dataN3[currentIndex];
+  const { knownStatus, markAsKnown, markAsNotKnown } = useVocabularyStatus({
+    level: LEVEL_N3,
+    currentCard: currentCard,
+  });
+  const goToPreviousCard = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+  };
+  const goToNextCard = () => {
+    setCurrentIndex((prevIndex) => Math.min(prevIndex + 1, dataN3.length - 1));
+  };
+
+  return (
+    <VocabularyPageContent
+      cardData={currentCard}
+      knownStatus={knownStatus}
+      level="5"
+      onPrevious={goToPreviousCard}
+      onNext={goToNextCard}
+      isPreviousDisabled={currentIndex === 0}
+      isNextDisabled={currentIndex === dataN3.length - 1}
+      onKnown={markAsKnown}
+      onNotKnown={markAsNotKnown}
+      currentIndex={currentIndex}
+      totalCards={dataN3.length}
+    />
+  );
+}
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Link, Route, Routes } from 'react-router-dom'
 import HomePage from './(routes)/Home'
 import Noken5Page from './(routes)/Noken5'
 import Noken4Page from './(routes)/Noken4'
+import Noken3Page from './(routes)/Noken3'
 
 function App() {
 
@@ -36,8 +37,8 @@ function App() {
           <Route path="/" element={<HomePage />} /> {/* Define el componente para la ruta principal */}
           <Route path="/noken-5" element={<Noken5Page />} /> {/* Define el componente para el nivel N5 */}
           <Route path="/noken-4" element={<Noken4Page />} />
-        {/* <Route path="/n3" element={<N3Level />} />
-        <Route path="/n2" element={<N2Level />} />
+        <Route path="/n3" element={<Noken3Page />} />
+        {/* <Route path="/n2" element={<N2Level />} />
         <Route path="/n1" element={<N1Level />} /> */}
         </Routes>
       </main>
