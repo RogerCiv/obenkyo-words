917d89229ddd8436ac7b3c659980356e16d720f3
RogerCiv <rogeliocivantos@gmail.com>
2025-03-26 20:59:34 +0100

    refactor fetchNokenVocabulary to remove console logs and streamline data fetching

--- src/data/vocabularyData.ts
+++ src/data/vocabularyData.ts
@@ -2,28 +2,25 @@ import { VocabularyCardType } from "../types/vocabularyTypes";
 import supabase from "../utils/supabase-client";
 
 async function fetchNokenVocabulary(level: string): Promise<VocabularyCardType[] | null> {
-  console.log('fetchN5Vocabulary level:', level);
   const pageSize = 1000;
   let allData: VocabularyCardType[] = [];
   let offset = 0;
-  
+
   while (true) {
     const { data, error } = await supabase
       .from(level)
       .select('*')
       .range(offset, offset + pageSize - 1);
-    
+
     if (error) {
       console.error('Error fetching N5 vocabulary:', error);
       return null;
     }
-    
+
     allData = [...allData, ...data];
     if (data.length < pageSize) break;
     offset += pageSize;
   }
-  
-  console.log('data:', allData);
   return allData;
 }
 
