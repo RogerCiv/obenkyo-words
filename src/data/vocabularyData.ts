0866db24752542a06e384d7fc43c6052b2096c39
RogerCiv <rogeliocivantos@gmail.com>
2025-03-26 20:32:28 +0100

    refactor fetchNokenVocabulary to paginate results and handle larger datasets

--- src/data/vocabularyData.ts
+++ src/data/vocabularyData.ts
@@ -3,14 +3,28 @@ import supabase from "../utils/supabase-client";
 
 async function fetchNokenVocabulary(level: string): Promise<VocabularyCardType[] | null> {
   console.log('fetchN5Vocabulary level:', level);
-  const { data, error } = await supabase.from(level).select('*');
-
-  if (error) {
-    console.error('Error fetching N5 vocabulary:', error);
-    return null;
+  const pageSize = 1000;
+  let allData: VocabularyCardType[] = [];
+  let offset = 0;
+  
+  while (true) {
+    const { data, error } = await supabase
+      .from(level)
+      .select('*')
+      .range(offset, offset + pageSize - 1);
+    
+    if (error) {
+      console.error('Error fetching N5 vocabulary:', error);
+      return null;
+    }
+    
+    allData = [...allData, ...data];
+    if (data.length < pageSize) break;
+    offset += pageSize;
   }
-  console.log('data:', data);
-  return data;
+  
+  console.log('data:', allData);
+  return allData;
 }
 
 export { fetchNokenVocabulary };
