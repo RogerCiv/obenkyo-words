04bd9631a687b8a850cfe80a3371c9320886cdd7
RogerCiv <rogeliocivantos@gmail.com>
2025-03-27 20:25:29 +0100

    feat: add logo image and update footer content for branding enhancement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,6 +29,7 @@ const Header = () => {
               <li><a>Item 3</a></li>
             </ul>
           </div>
+        <img src="/logo.jpeg" alt="Noken Vocabulary Logo" className="rounded-full" width="50" height="50" />
           <a className="btn btn-ghost text-xl">Noken <span className="text-accent">Vocabulary</span></a>
         </div>
         <div className="navbar-center hidden lg:flex">
