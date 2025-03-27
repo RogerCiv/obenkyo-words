66b10fc6a91041168f0e4c2bc8f62b4547182627
RogerCiv <rogeliocivantos@gmail.com>
2025-03-27 14:46:13 +0100

    implement flip card functionality in Card component; enhance VocabularyCardDisplay layout and styling for improved responsiveness

--- src/components/VocabularyCardDisplay.tsx
+++ src/components/VocabularyCardDisplay.tsx
@@ -18,14 +18,29 @@ interface VocabularyCardDisplayProps {
 export default function VocabularyCardDisplay(props: VocabularyCardDisplayProps) {
   const { cardData, knownStatus, level, isNextDisabled, isPreviousDisabled, onNext, onPrevious } = props;
   return (
-    <div className="flex flex-col items-center justify-center h-full gap-10">
-      <h1 className="text-5xl">Vocabulario <span className="text-accent font-semibold">Noken {level}</span></h1>
-      <div className="flex flex-col gap-4">
+    <div className="flex flex-col items-center justify-center  w-full px-4 py-8 gap-6 md:gap-10">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl text-center">
+        Vocabulario <span className="text-accent font-semibold">Noken {level}</span>
+      </h1>
+      <div className="w-full max-w-3xl mx-auto">
+        <div
+          className={`w-full aspect-[4/3] sm:aspect-[3/2] md:aspect-[2/1] mb-4 md:mb-8 rounded-lg shadow-lg transition-colors duration-300 p-3
+          ${knownStatus === true
+              ? "bg-success/10 border-2 border-success"
+              : knownStatus === false
+                ? "bg-error/10 border-2 border-error"
+                : "bg-base-200 border-2 border-base-300"
+            }`}
+        >
 
-        <div className={`w-xl h-96 mb-8 p-4 rounded shadow-lg transition-colors duration-300
-        ${knownStatus === true ? 'bg-success border border-success-content' : knownStatus === false ? 'bg-error border border-error-content' : 'bg-base-content'}`}>
+ 
+            <Card
+              expression={cardData.expression}
+              meaning={cardData.meaning}
+              reading={cardData.reading}
+              status={knownStatus}
+            />
 
-          <Card expression={cardData.expression} meaning={cardData.meaning} reading={cardData.reading} status={knownStatus} />
 
           <IconsNavigate
             knownStatus={knownStatus}
