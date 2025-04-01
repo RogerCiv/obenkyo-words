c0360c3ee9a3bb64285a76f18438e2e4141e4eef
RogerCiv <rogeliocivantos@gmail.com>
2025-04-01 12:35:11 +0200

    feat: add AlertMessage component to display construction notice in Layout

---
+++ src/components/AlertMessage.tsx
@@ -0,0 +1,14 @@
+
+
+export default function AlertMessage() {
+
+  return (
+    <div className="sticky top-0 z-50">
+      <div
+        role="alert"
+        className="alert alert-info rounded-none flex items-center justify-center text-center">
+        <span>🚧 La web está en construcción. ¡Pronto novedades! 🚧</span>
+      </div>
+    </div>
+  );
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import AlertMessage from './AlertMessage'
 
 const Layout = () => (
   <div className='flex flex-col bg-base-200'>
+    <AlertMessage />
     <Header />
     <main className='flex-grow'>
       <Outlet />
     </main>
-   <Footer />
+    <Footer />
   </div>
 )
 
