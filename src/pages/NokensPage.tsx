83e44d92d9282396f7ad3bef44a16b59c1db973c
RogerCiv <rogeliocivantos@gmail.com>
2025-04-02 19:50:55 +0200

    fix: adjust layout and grid configuration in NokensPage for improved responsiveness

--- src/pages/NokensPage.tsx
+++ src/pages/NokensPage.tsx
@@ -10,9 +10,9 @@ export default function NokensPage() {
   ];
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-screen flex flex-col items-center justify-center py-14 xl:py-0">
       <h1 className="text-4xl font-bold mb-6">Selecciona tu Noken</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 ">
         {niveles.map((nivel) => (
           <CardNokenLevels key={nivel.title} levelCard={nivel} />
         ))}
