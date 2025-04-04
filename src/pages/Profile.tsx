e18f4d0bd9bcf3eae00879393585c4939d4d1dab
RogerCiv <rogeliocivantos@gmail.com>
2025-04-04 12:45:26 +0200

    feat: integrate StudyCalendar component and useVocabularyStatus hook in Profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,20 @@
+"use client"
 
 import { NokenCharts } from "@/components/NokenCharts";
+import { StudyCalendar } from "@/components/StudyCalendar";
 import { useAuth } from "@/hooks/useAuth";
+import useVocabularyStatus from "@/hooks/useVocabularyStatus";
 
 export default function Profile() {
   const { user } = useAuth();
+  const { studyDays } = useVocabularyStatus();
+
+  console.log("studyDays", studyDays)
   return (
     <section className="container mx-auto py-10 px-4 md:px-0">
       <h1>PROFILE de {user?.email}</h1>
+      <div className=""></div>
+      <StudyCalendar dates={studyDays} />
       <NokenCharts />
     </section>
   )
