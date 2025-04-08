fc7b1086688549328288d16c127dba0f9619eb52
RogerCiv <rogeliocivantos@gmail.com>
2025-04-08 12:47:29 +0200

    feat: add DetailsAccount component for user account management

---
+++ src/components/DetailsAccount.tsx
@@ -0,0 +1,40 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Link } from "react-router-dom"
+import { Button } from "./ui/button"
+import { User } from "@supabase/supabase-js"
+
+interface DetailsAccountProps {
+  user: User
+  handleLogout: () => void
+}
+
+export default function DetailsAccount(props: DetailsAccountProps) {
+  const { user, handleLogout } = props
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="cursor-pointer">Detalles Cuenta</DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{user.user_metadata.display_name || "Usuario"}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link to="/profile">Perfil</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link to="/ajustes">Ajustes</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Button variant="destructive" size="sm" onClick={handleLogout} className="cursor-pointer">
+            Logout
+          </Button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
