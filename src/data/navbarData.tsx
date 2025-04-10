14471ab653e93b711dda52c05b46f36699fcf10b
RogerCiv <rogeliocivantos@gmail.com>
2025-04-10 12:07:56 +0200

    fix: remove unused Niveles Noken menu item and its associated data

--- src/data/navbarData.tsx
+++ src/data/navbarData.tsx
@@ -1,4 +1,3 @@
-import { Zap, Sunset, Trees, Book } from "lucide-react";
 
 export const defaultNavbarData = {
   menu: [
@@ -6,42 +5,6 @@ export const defaultNavbarData = {
     {
       title: "Nokens",
       url: "/nokens",
-    },
-    {
-      title: "Niveles Noken",
-      url: "#",
-      items: [
-        {
-          title: "Noken 5",
-          description: "Get all the answers you need right here",
-          icon: <Zap className="size-5 shrink-0" />,
-          url: "/nokens/noken-5",
-        },
-        {
-          title: "Noken 4",
-          description: "We are here to help you with any questions you have",
-          icon: <Sunset className="size-5 shrink-0" />,
-          url: "#",
-        },
-        {
-          title: "Noken 3",
-          description: "Check the current status of our services and APIs",
-          icon: <Trees className="size-5 shrink-0" />,
-          url: "#",
-        },
-        {
-          title: "Noken 2",
-          description: "Our terms and conditions for using our services",
-          icon: <Book className="size-5 shrink-0" />,
-          url: "#",
-        },
-        {
-          title: "Noken 1",
-          description: "Our terms and conditions for using our services",
-          icon: <Book className="size-5 shrink-0" />,
-          url: "#",
-        },
-      ],
     },
   ],
   auth: {
