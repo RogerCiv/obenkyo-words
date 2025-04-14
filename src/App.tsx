91d4898c261f5e6a9f711318fae314e326f8e7ab
RogerCiv <rogeliocivantos@gmail.com>
2025-04-14 13:09:46 +0200

    feat: refactor routing by moving routes to a separate Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,15 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/Home'
-import Noken5Page from './pages/Noken5'
-import Noken4Page from './pages/Noken4'
-import Noken3Page from './pages/Noken3'
-import ProtectedRoutes from './utils/protectedRoutes'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Layout from './components/Layout'  
-import NotFound from './pages/NotFound'
-import NokensPage from './pages/NokensPage'
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './routes/router';
 import { AuthProvider } from './context/AuthProvider'
-import Noken2Page from './pages/Noken2'
-import Noken1Page from './pages/Noken1'
-import Profile from './pages/Profile'
-import PublicRoutes from './utils/PublicRoutes'
-import SettingsPage from './pages/SettingsPage'
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route 
-              path="/login" 
-              element={
-                <PublicRoutes>
-                  <Login />
-                </PublicRoutes>
-              } 
-            />
-            <Route 
-              path="/register" 
-              element={
-                <PublicRoutes>
-                  <Register />
-                </PublicRoutes>
-              } 
-            />
-            
-            <Route element={<ProtectedRoutes />}>
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/ajustes" element={<SettingsPage />} />
-            </Route>
-            
-            <Route path="/nokens">
-              <Route index element={<NokensPage />} />
-              <Route element={<ProtectedRoutes />}>
-                <Route path="noken-5" element={<Noken5Page />} />
-                <Route path="noken-4" element={<Noken4Page />} />
-                <Route path="noken-3" element={<Noken3Page />} />
-                <Route path="noken-2" element={<Noken2Page />} />
-                <Route path="noken-1" element={<Noken1Page />} />
-              </Route>
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
+        <Router />
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
