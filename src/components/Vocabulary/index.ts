fd7b2ed374b9a571bc681aa8cd0cabf2c64a3fe7
RogerCiv <rogeliocivantos@gmail.com>
2025-04-14 13:31:01 +0200

    feat: export VocabularyLeftSection and VocabularyRightSection components

--- src/components/Vocabulary/index.ts
+++ src/components/Vocabulary/index.ts
@@ -2,4 +2,7 @@ export * from "./VocabularyCardDisplay"
 export * from "./VocabularyNavItems"
 export * from "./VocabularyPageContent"
 export * from "./VocabularyWordList"
+export * from "./VocabularyLeftSection"
+export * from "./VocabularyRightSection"
+
 export * from "./Card"
